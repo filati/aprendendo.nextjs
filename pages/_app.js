86c7cee7819052070f2cb884f3daeb2e9e35aedd
Fabio Fila <contato@fabiofila.com.br>
2021-08-11 23:06:53 -0300

    WIP: Menu inicial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import 'tailwindcss/tailwind.css'
+import Menu from '../components/menu'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({Component, pageProps}) {
+
+
+    return (
+        <>
+            <Head>
+                <title>Meu 1º app no next.js |</title>
+            </Head>
+            <Menu></Menu>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
